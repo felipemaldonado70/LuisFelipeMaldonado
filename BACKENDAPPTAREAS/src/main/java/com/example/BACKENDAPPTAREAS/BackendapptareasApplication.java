0aaf20c02d43902dc0e58d4cc064feae00101572
felipemaldonado <felipe.maldonado41@gmail.com>
2020-11-19 15:40:24 -0500

    proyecto tareas V1

---
+++ BACKENDAPPTAREAS/src/main/java/com/example/BACKENDAPPTAREAS/BackendapptareasApplication.java
@@ -0,0 +1,13 @@
+package com.example.BACKENDAPPTAREAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendapptareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendapptareasApplication.class, args);
+	}
+
+}
---
+++ BACKENDAPPTAREAS/src/test/java/com/example/BACKENDAPPTAREAS/BackendapptareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BACKENDAPPTAREAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendapptareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
